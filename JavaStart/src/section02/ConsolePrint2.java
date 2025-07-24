f45a24c7ac722beed72b6ccb9f14b58ad1e01691
jik0218 <jik0218@exmple.com>
2025-07-24 22:49:44 +0900

    JavaStart

---
+++ JavaStart/src/section02/ConsolePrint2.java
@@ -0,0 +1,11 @@
+package section02;
+
+public class ConsolePrint2 {
+	public static void main(String[] args) {
+		// 괄호() 안의 데이터를 콘솔창에 입력
+	    System.out.println("Welcome.");
+		// "Welcome." 문자열 옆에 "Java World" 문자열을 출력
+		System.out.println("Java World");
+	}
+
+}
