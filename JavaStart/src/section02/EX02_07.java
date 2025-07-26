791108c86ea9bb57a798f1d6534f1f6c67d6507a
jik0218 <jik0218@exmple.com>
2025-07-26 21:51:25 +0900

    JavaStart

---
+++ JavaStart/src/section02/EX02_07.java
@@ -0,0 +1,11 @@
+package section02;
+
+public class EX02_07 {
+	public static void main(String[] args) {
+		int stdentAge; //학생의 나이를 저장할 수 있는 변수 선언
+		stdentAge = 20; // 변수에 값을 대입(저장)
+		
+		System.out.println(stdentAge);
+	}
+
+}
