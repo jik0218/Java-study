cd182d4cb3daa5fc7cf356d4733d32f470a33ec5
jik0218 <jik0218@exmple.com>
2025-08-31 17:11:25 +0900

    JavaStart

--- Java/src/chapter02/Z_Ansser.java
+++ Java/src/chapter02/Z_Ansser.java
@@ -10,16 +10,58 @@ public class Z_Ansser {
 		// 줄의 순서 : 1, 2, 3, 4, 5
 		// 별의 개수 : 5, 4, 3, 2, 1
 		// : 정답 작성
+//		for(int i = 0; i < 5; i++) {
+//			for(int j = 0; j < 5 -i; j++) {
+//				System.out.print("*");
+//			}
+//			
+//			for(int k =0; k < i; k++) {
+//				System.out.print(" ");
+//			}
+//			System.out.println();
+//		}
+		
+		int height = 5; //별의 높이를 고정값 5로 설정
+		
+		// 첫 번째 for 문 : 줄(row)을 제어 - 5줄 출력
+		for(int i = height; i >= 1; i--) {
+			
+			// 두 번째 for문 : 각 줄에 별을 출력
+			for(int j = 1; j <= i; j++) {
+				System.out.print("*"); // 별 출력
+			}
+			System.out.println(); // 줄 바꿈
+		}
 		
 		System.out.println("---------------------------------------------");
 		
-		//	*****
+		//  *****
 		//	 ****
 		//	  ***
 		//	   **
 		//	    *
 		
 		// : 정답 작성
+		int num = 5;
+		
+		// 첫 번째 for 문 : 줄(row)을 제어 - 5줄 출력
+		for(int i = num; i >=1; i--) {
+			// i는 5, 4, 3, 2, 1
+			
+			// 두 번째 for 문 : 줄의 시작 부분에 공백을 추가 (오른쪽 정렬)
+			// - 0, 1, 2, 3, 4
+			for(int j = 1; j <= num -i; j++) {
+				System.out.print(" ");
+			}
+			
+			// 세 번째 for 문 : 별을 추가
+			// - 5, 4, 3, 2, 1
+			for(int k =1; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println(); // 줄 바꿈
+		}
+		
 	}
 	
 
---
+++ JavaStart/src/section05/EX05_06.java
@@ -0,0 +1,16 @@
+package section05;
+
+public class EX05_06 {
+	public static void main(String[] args) {
+		int sum = 0;
+		int i = 1;
+		
+		do {
+			sum += i;
+			i++;
+		}while(i <= 10);
+		
+		System.out.println("합 : " + sum);
+	}
+
+}
